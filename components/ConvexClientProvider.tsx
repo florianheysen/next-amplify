ee4ffa884a9cbb4594bd6537205e797b0cdf5205
Florian Heysen <heysen.florian@gmail.com>
2024-10-02 20:54:41 +0200

    first commit

---
+++ components/ConvexClientProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ReactNode } from "react";
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convexQueryClient = new ConvexQueryClient(convex);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryKeyHashFn: convexQueryClient.hashFn(),
+      queryFn: convexQueryClient.queryFn(),
+    },
+  },
+});
+convexQueryClient.connect(queryClient);
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
+  return (
+    <ConvexProvider client={convex}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ConvexProvider>
+  );
+}
