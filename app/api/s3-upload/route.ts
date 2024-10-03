ee8fc86e94f7c17b0d75ddfbc3d0390aa0749422
Florian Heysen <heysen.florian@gmail.com>
2024-10-03 21:20:19 +0200

    add more logs

--- app/api/s3-upload/route.ts
+++ app/api/s3-upload/route.ts
@@ -1,9 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { POST as originalPost } from "next-s3-upload/route";
 
 export async function POST(request: NextRequest) {
   console.log(
     "------------------------------------- S3 UPLOAD -------------------------------------"
   );
+  console.log("bucket:", process.env.S3_UPLOAD_BUCKET);
+  console.log("region:", process.env.S3_UPLOAD_REGION);
   return originalPost(request);
 }
